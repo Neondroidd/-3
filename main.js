334bb1a773e13bc6fe8f2c61db17c62445cd54d4
Neondroidd <apakabarhai514@gmail.com>
2023-11-20 01:34:43 +0800

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-onload = () =>{
-        document.body.classList.remove("container");
+onload = () => {
+  document.body.classList.remove("container");
+  const audio = new Audio("./Music/Main.mp3");
+  audio.play();
 };
