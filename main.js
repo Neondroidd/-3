3e2fcd05720b7d42f91a0a815c6103590e30f29a
Neondroidd <91836929+Neondroidd@users.noreply.github.com>
2024-02-22 21:06:43 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
- window.onload = () => {
-            // Prompt the user for their name
-            const userName = prompt("Masukkan namamu:");
-
-            // Display the user's name in the "Name" span
-            const nameElement = document.querySelector('.Name');
-            if (nameElement) {
-                nameElement.textContent = userName || 'Tamu';
-            }
-
-      const audio = new Audio("./Music/Main.mp3");
-      audio.play();
-    };
+onload = () => {
+  document.body.classList.remove("container");
+  const audio = new Audio("./Music/Main.mp3");
+  audio.play();
+};
